53e8ee251b6e6389d4493aa0fc59fa7da035a7f8
Wagner Leal <wagnerlcta@gmail.com>
2017-07-13 20:48:33 -0300

    Task 5 finished

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -5,6 +5,7 @@ import {
     EntityDetailsComponent, ShowLineComponent,
     ListingTableComponent
 } from 'angularm';
+import { CssTableLineComponent } from "app/widgets/css.table.line";
 
 export let defineRules = (angularm: AngularmService) => {
     angularm
@@ -12,7 +13,11 @@ export let defineRules = (angularm: AngularmService) => {
         .dptr('form_line', FormLineComponent, { inputType: 'text' })
         .dpr('show_line', ShowLineComponent)
         .detr('list_entities', ListingTableComponent)
-        .der('table_line', EntityLineComponent)
+        .er('table_line', 'bug', CssTableLineComponent, {lineClass: 'w3-hover-green'})
+        .er('table_line', 'issue', CssTableLineComponent, {lineClass: 'w3-hover-green'})
+        .er('table_line', 'product', CssTableLineComponent, {lineClass: 'w3-hover-green'})
+        .er('table_line', 'campaign', CssTableLineComponent, {lineClass: 'w3-hover-green'})
+        .der('table_line', CssTableLineComponent)
         .der('show_entity', EntityDetailsComponent)
         .detr('create_form', CreateEntityComponent)
         .der('edit_form', EditEntityFormComponent)
