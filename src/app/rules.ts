48968c2d075dae837c959be3d8a0088882ca5c94
Wagner Leal <wagnerlcta@gmail.com>
2017-07-15 11:59:46 -0300

    Task 11 finished

--- src/app/rules.ts
+++ src/app/rules.ts
@@ -5,9 +5,13 @@ import {
     EntityDetailsComponent, ShowLineComponent,
     ListingTableComponent
 } from 'angularm';
+import { TextareaFormLineComponent } from "app/widgets/textarea.formline";
+
 
 export let defineRules = (angularm: AngularmService) => {
-    angularm
+    angularm 
+        .ptr('form_line', '*','description', null, TextareaFormLineComponent, {rows: 8, cols: 50})
+        .ptr('form_line', '*','summary', null, TextareaFormLineComponent, {rows: 5, cols: 50})
         .dptr('form_line', FormLineComponent, { inputType: 'text' })
         .dpr('show_line', ShowLineComponent)
         .detr('list_entities', ListingTableComponent)
@@ -15,6 +19,7 @@ export let defineRules = (angularm: AngularmService) => {
         .der('show_entity', EntityDetailsComponent)
         .detr('create_form', CreateEntityComponent)
         .der('edit_form', EditEntityFormComponent)
+        
         .pr('edit_form_line', '*', 'code', null, EditFormLineComponent, { inputType: 'number' })
         .dpr('edit_form_line', EditFormLineComponent, { inputType: 'text' });
 };
